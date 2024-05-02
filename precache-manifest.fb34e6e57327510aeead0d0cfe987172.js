1ab7bb8b39ded874f8ab1128f77e87b71142e1e8
RasaIulian <ilie.rasa@gmail.com>
2024-05-02 20:04:29 +0300

    Updates

---
+++ precache-manifest.fb34e6e57327510aeead0d0cfe987172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d1d715c72553bc61c6011d28bd61df",
+    "url": "/ShoppingList/index.html"
+  },
+  {
+    "revision": "ee819f547b0e3b48bd55",
+    "url": "/ShoppingList/static/css/main.9dcdd30d.chunk.css"
+  },
+  {
+    "revision": "aa0c04ed82f3b4dbdbed",
+    "url": "/ShoppingList/static/js/2.dd6ba0b0.chunk.js"
+  },
+  {
+    "revision": "ee819f547b0e3b48bd55",
+    "url": "/ShoppingList/static/js/main.e325de90.chunk.js"
+  },
+  {
+    "revision": "4b2c9ec348ee56ff6581",
+    "url": "/ShoppingList/static/js/runtime~main.4013c62d.js"
+  }
+]);
